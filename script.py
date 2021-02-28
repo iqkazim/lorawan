49153cf061b4cc8b87eaf047a28b05b5eadf9728
iqkazim <76489678+iqkazim@users.noreply.github.com>
2021-02-28 16:11:42 +0200

    Add files via upload
    Script to run the simulations and save the results

--- script.py
+++ script.py
@@ -11,14 +11,7 @@ for filename in os.listdir(os.getcwd()):
          p = subprocess.Popen(["bash run_aloha.sh",filename], shell=True,stdout=f)#stdout=subprocess.PIPE), stderr=subprocess.PIPE)
          stdout_results, stderr_results = p.communicate()
         
-         #re.findall('^[0-9].*',f,re.MULTILINE)
-"""with open("SF.csv","r+") as d:
-         for line in d:
-             if len(line) > 0 and line [0].isdigit():
-                print(line)
-                d.write(line)
-def removetext():
-"""
+
 input = open("SF.csv","r")
 output = open("SF_7.csv","w")
 
