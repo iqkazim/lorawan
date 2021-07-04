75ab44559ffe55f8d310dc0a68f0bd98fae8d4ad
iqkazim <76489678+iqkazim@users.noreply.github.com>
2021-07-04 16:08:29 +0200

    Update run_aloha.sh

--- run_aloha.sh
+++ run_aloha.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 reps=`seq 1`
-nDevices=`seq 10 10 20`
+#nDevices=`seq 10 10 20`
+loss=`seq 3 0.5 5'
 
-for r in $reps
+for p in $loss
 do
 
-      for n in $nDevices
+      for r in $reps
       do
-             ./waf --run "aloha-throughput --nDevices=$n --RngRun=$r"
+             ./waf --run "aloha-throughput --nDevices=7515 --pathloss=$p --RngRun=$r"
       done
 done
